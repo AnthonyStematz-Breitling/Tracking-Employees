1ac8bf2e2c9529e4b25c27f87bd96db9adea1dda
ANTHONY STEMATZ-BREITLING <anthonystematz-breitling@ANTHONYs-MacBook-Pro.local>
2020-08-28 12:31:52 -0500

    created two more tables, and created unique ids for each row of the table

--- employees.sql
+++ employees.sql
@@ -1,7 +1,23 @@
 DROP DATABASE IF EXISTS work-force_db;
 CREATE DATABASE work-force_db;
 
+CREATE TABLE department(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+)
+CREATE TABLE position(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+)
+
 CREATE TABLE employees (
-    
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    firstname VARCHAR(25) NOT NULL,
+    lastname VARCHAR(35) NOT NULL,
+    PRIMARY KEY (id)
+
+
 )
 
