e6bf8c204d3bcb3125f464f02fc16acda83461fa
ANTHONY STEMATZ-BREITLING <stembreit@gmail.com>
2020-08-29 12:57:01 -0500

    added cases to the switch statement

--- inquires.js
+++ inquires.js
@@ -3,26 +3,55 @@ const inquirer = require("inquirer")
 function initiate(){
 inquirer.prompt(
     {
-        type:"",
+        type:"list",
         message: "What would you like to do?",
         choices: [
-            {message: "Add an Employee", name: ""},
+             "Create Employee",
             //{message: "Delete an Employee", name: ""},
-            {message: "View all Employees", name: ""},
-            {message: "Creaet Department", name:""},
+            "View All Employees",
+            "Create Department",
             //{message: "Delete a Department"},
-            {message:"Create Role", name:""},
+            "Create Role",
             //{message: "Delete a role"},
-            {message: "View Employees by Department", name: ""},
-            {message: "View Employees by Role", name: ""},
+            "View Employees by Department", 
+            "View Employees by Role", 
             //{message: "View Employees by Manager", name: ""},
-            {message: "Change an Employees Role", name: ""},
+            "Change an Employees Role", 
             //{message: "Change an Employees Manager", name: ""}
         ]
     }
 
 )}
 
-function Choice(){
-    switch
+//function with switch statement
+function choice(data){
+    switch(data){
+        case "Create Employee":
+        break;
+
+        case "Create Role":
+        break;
+
+        case "Create Department":
+        break;
+
+        case "View All Employees":
+        break;
+
+        case "View Employees by Department":
+        break;
+
+        case "View Employees by Role":
+        break;
+
+        case "Change an Employees Role":
+        break;
+    }
+
 }
+
+
+//Create is going to have a second inquirer prompt and an INSERT mysql function
+//View is going to select certain parts of the table and then present those parts
+//Change is going to find an employee by unique id and then change the chosen column as desired
+//Delete is going to find a row by unique id and then remove that row 
