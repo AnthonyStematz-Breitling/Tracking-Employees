88f3631aa300c06f0f2c54403d58b887c26f443b
ANTHONY STEMATZ-BREITLING <stembreit@gmail.com>
2020-09-01 13:53:27 -0500

    added a seeds file for database

---
+++ workforce_seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO role (title, salary)
+VALUES(Manager, 70000)
+VALUES(Phone Operator, 50000)
+VALUES(Intern, 35000)
+
+INSERT INTO department(name)
+VALUES(Human Resources)
+VALUES(Billing)
+VALUES(Management)
